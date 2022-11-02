b18ceb542b2014dcd7ae51fb3074835ab3b67683
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-11-02 17:08:19 +0100

    Corretti metodi, stampa dati in console

--- Program.cs
+++ Program.cs
@@ -14,10 +14,15 @@
 //BONUS: create un metodo che restituisca il codice con un pad left di 0 per arrivare a 8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
 
 
-Prodotto prodottoEsempio = new Prodotto("romanzo", "un thriller mozzafiato", 15.99);
+Prodotto prodottoEsempio = new Prodotto("Romanzo giallo", "Un thriller mozzafiato.", 15.99);
 
-
-
+Console.WriteLine("Codice ISBN: " + prodottoEsempio.GetCodice());
+Console.WriteLine("Titolo del libro: " + prodottoEsempio.GetNome());
+Console.WriteLine("Descrizione: " + prodottoEsempio.GetDescrizione());
+Console.WriteLine("Prezzo: " + prodottoEsempio.GetPrezzo() + " euro");
+Console.WriteLine("Iva applicabile: " + prodottoEsempio.GetIva() + " %");
+Console.WriteLine("Prezzo con Iva: " + prodottoEsempio.GetPrezzoIvato() + " euro");
+Console.WriteLine(prodottoEsempio.GetNomeCodice());
 
 
 // classe prodotto
@@ -30,18 +35,68 @@ public class Prodotto
      string nome;
      string descrizione;
      double prezzo;
-     double iva;
+     int iva;
  
 
     // costruttore
     public Prodotto(string nome, string descrizione, double prezzo)
     {
         Random random = new Random();
-        codice = random.Next(1, 10001);
+        codice = random.Next(1, 12345678);
         this.nome = nome;
         this.descrizione = descrizione;
         this.prezzo = prezzo;
-        iva = 0.22;
+        iva = 22;
+    }
+
+    // methods
+
+    public int GetCodice()
+    {
+        return codice;
+    }
+    public void SetNome(string nome)
+    {
+        this.nome = nome;
+    }
+    public string GetNome()
+    {
+        return nome;
+    }
+    public void SetDescrizione(string descrizione)
+    {
+        this.descrizione = descrizione;
+    }
+    public string GetDescrizione()
+    {
+        return descrizione;
+    }
+    public void SetPrezzo(double prezzo)
+    {
+        this.prezzo = prezzo;
+    }
+    public double GetPrezzo()
+    {
+        return prezzo;
+    }
+    public void SetIva(int iva)
+    {
+        this.iva = iva;
+    }
+
+    public int GetIva()
+    {
+        return iva;
+    }
+
+    public double GetPrezzoIvato()
+    {
+        return prezzo + (prezzo / 100 * iva);
+    }
+    public string GetNomeCodice()
+    {
+        return "Titolo del libro: " + nome + " - " + "Codice: " + codice;
     }
 
 }
+
